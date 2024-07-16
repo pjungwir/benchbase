d7ed616951de9aaf8e98ec8fb68f300c81dbffe8
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-07-16 10:20:44 -0700

    Add SelectEmployeesWithoutPosition (antijoin)

---
+++ src/main/java/com/oltpbenchmark/benchmarks/temporal/procedures/SelectEmployeesWithoutPosition.java
@@ -0,0 +1,50 @@
+package com.oltpbenchmark.benchmarks.temporal.procedures;
+
+import com.oltpbenchmark.api.Procedure;
+import com.oltpbenchmark.api.SQLStmt;
+import com.oltpbenchmark.benchmarks.temporal.DateRange;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+/**
+ * Finds employees at t who have no position.
+ *
+ * <p>This lets us benchmark antijoins.
+ */
+public class SelectEmployeesWithoutPosition extends Procedure {
+  public final SQLStmt selectEmployeesWithoutPosition =
+      new SQLStmt(
+          ""
+              + "SELECT  e.id, UNNEST(CASE WHEN j.valid_at IS NULL "
+              + "                          THEN multirange(e.valid_at) "
+              + "                          ELSE multirange(e.valid_at) - j.valid_at "
+              + "                     END)::text AS valid_at, "
+              + "        e.name, e.salary "
+              + "FROM employees e "
+              + "LEFT JOIN ("
+              + "  SELECT  p.employee_id, range_agg(p.valid_at) AS valid_at "
+              + "  FROM    positions p "
+              + "  GROUP BY p.employee_id "
+              + ") AS j "
+              + "ON e.id = j.employee_id AND e.valid_at && j.valid_at "
+              + "WHERE e.valid_at @> ?::date");
+
+  public void run(Connection conn, LocalDate asof) throws SQLException {
+    try (PreparedStatement stmt = this.getPreparedStatement(conn, selectEmployeesWithoutPosition)) {
+      stmt.setDate(1, Date.valueOf(asof));
+      try (ResultSet rs = stmt.executeQuery()) {
+        while (rs.next()) {
+          int id = rs.getInt(1);
+          DateRange validAt = DateRange.parse(rs.getString(2));
+          assert validAt.from != null;
+          assert asof.isEqual(validAt.from) || asof.isAfter(validAt.from);
+          assert validAt.til == null || asof.isBefore(validAt.til);
+        }
+      }
+    }
+  }
+}
